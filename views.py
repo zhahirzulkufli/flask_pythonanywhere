f6e2e8e3da208602c114040853f7e21a1eeb33a5
zhahirzulkufli <zhahirzulkufliyulan@gmail.com>
2018-08-01 16:01:04 +0800

    Update views.py

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
-from flask import request, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__)
 
 @app.route("/")
 def base():
